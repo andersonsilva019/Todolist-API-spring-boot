16019bea58fa3b0f14e620e99b737cd5e03d1510
Anderson Silva <andersonnsilva@alu.ufc.br>
2023-10-13 09:52:05 -0300

    Api todolist

---
+++ src/main/java/br/com/andersonsilva019/todolist/task/TaskModel.java
@@ -0,0 +1,41 @@
+package br.com.andersonsilva019.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+
+    private UUID userId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    private String priority;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50)
+            throw new Exception("The title must be less than 50 characters");
+        this.title = title;
+    }
+
+}
